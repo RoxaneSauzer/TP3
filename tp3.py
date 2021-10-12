d5d60d7803c2e1872769254c61c4b7de96534c8f
roxanesauzer <roxane.sauzer@etu.univ-amu.fr>
2021-10-12 18:01:55 +0200

    tp3 prog

--- tp3.py
+++ tp3.py
@@ -19,18 +19,20 @@ class Heap :
         return (i-1)//2
 
     def swap(self,i,j):
-        assert (i<len(self.heap_list))
-        assert (j<len(self.heap_list))
+        assert (i<=len(self.heap_list))
+        assert (j<=len(self.heap_list))
         self.heap_list[i],self.heap_list[j] = self.heap_list[j],self.heap_list[i]
         return self.heap_list
 
     def percolate_up(self, n_index):
-        print(str(n_index))
+        if len(self.heap_list)==1:
+            return self.heap_list
         if n_index==0:
-            n_index=self.heap_list.index(self.heap_list[-1])
+            n_index=len(self.heap_list)-1
         if self.heap_list[n_index]<self.heap_list[self.get_parent(n_index)]:
+            a= self.get_parent(n_index)
             self.swap(n_index, self.get_parent(n_index))
-            return self.percolate_up(self.get_parent(n_index))
+            return self.percolate_up(a)
         return self.heap_list
 
     def insert(self,n):
@@ -40,30 +42,58 @@ class Heap :
 
     def min_child(self,i):
         min=0
-        if self.get_left_child(i)<self.get_right_child(i):
+        if len(self.heap_list)<2*i+1:
+            print('no child to compare')
+        if self.heap_list[self.get_left_child(i)]<self.heap_list[self.get_right_child(i)]:
             min=self.get_left_child(i)
         else:
             min=self.get_right_child(i)
+        return min
 
 
     def percolate_down(self, i):
+        print(i)
 
+        if len(self.heap_list)<2*i+1:
+            print('no child to compare')
+            return self.heap_list
+        print(self.min_child(i))
         if self.heap_list[self.min_child(i)]<=self.heap_list[i]:
+            a= self.min_child(i)
             self.swap(i, self.min_child(i))
-            return self.percolate_down(self.min_child(i))
+            print(self.heap_list)
+            return self.percolate_down(a)
         return self.heap_list
 
 
-
     def extract(self):
-        self.heap_list.remove(self.heap_list[0])
-        last=self.heap_list.pop()
-        self.heap_list=[last]+self.heap_list
-        self.percolate_down(0)
+        self.swap(0,len(self.heap_list)-1)
+        self.heap_list.pop()
+        return self.percolate_down(0)
+
+    def delete(self, i):
+        new_list=[]
+        for k in range(i, len(self.heap_list)):
+            print(self.heap_list[k])
+            new_list.append(self.heap_list[k])
+
+        for k in range(len(new_list)):
+            self.heap_list.pop()
+        tas=Heap(new_list)
+        print(tas.heap_list)
+        tas.extract()
+        for k in range(len(new_list)):
+            self.heap_list.append(tas.heap_list[k])
+        return self.heap_list
 
 
+    def build(self, list):
+        self.heap_list=[]
+        for i in list:
+            self.insert(i)
+        return self.heap_list
 
 
-tas=Heap([1,5,8,3])
-
-print(tas.extract())
+l=[1,4,2,3,5]
+tas=Heap([])
+print(tas.build(l))
